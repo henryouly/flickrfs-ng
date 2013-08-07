827120b4722e876517fbe3baaede1e6dee0c652c
root <root@EMC-lifeline.localhost>
2013-08-07 22:32:10 +0800

    Switch back to __getitem__ and __setitem__ for easy code logic

--- i_node.py
+++ i_node.py
@@ -16,19 +16,11 @@ class INode(object):
     for k in kwargs:
       self.attr[k] = kwargs[k]
 
-  def __getattr__(self, key):
-    try:
-      return self.__dict__['attr'][key]
-    except KeyError:
-      raise AttributeError(key)
+  def __getitem__(self, key):
+    return self.attr.get(key)
 
-  def __setattr__(self, key, value):
-    if not self.__dict__.has_key(key) and key == 'attr':
-      object.__setattr__(self, key, value)
-    elif self.__dict__.has_key(key):
-      object.__setattr__(self, key, value)
-    else:
-      self.attr[key] = value
+  def __setitem__(self, key, value):
+    self.attr[key] = value
 
   def getattrs(self):
     return self.attr
@@ -40,6 +32,6 @@ class INode(object):
   def mknod(self, **kwargs):
     node = INode(**kwargs)
     if kwargs.get('st_mode', 0) & S_IFDIR:
-      node.st_nlink = 2
-    self.st_nlink += 1
+      node['st_nlink'] = 2
+    self['st_nlink'] += 1
     return node
