2b2fe13bf83b05a1ea8c99dc9d50b01780139b04
Henry Ou <henryou@dhcp-172-30-157-131.bej.corp.google.com>
2013-08-12 18:04:06 +0800

    Add test_photo_list

---
+++ test_photo_list.py
@@ -0,0 +1,14 @@
+file_list = [
+    'http://farm6.staticflickr.com/5516/9433700758_2cd5dd2d9b_o.jpg',
+    'http://farm8.staticflickr.com/7332/9430929735_6082fd6329_o.jpg',
+    'http://farm3.staticflickr.com/2860/9430933391_7edf2885d3_o.jpg',
+    'http://farm8.staticflickr.com/7445/9433710310_229c52ce5e_o.jpg',
+    'http://farm8.staticflickr.com/7305/9433713274_294ebbd92c_o.jpg',
+    'http://farm3.staticflickr.com/2879/9433716666_004d2d5c41_o.jpg',
+    'http://farm8.staticflickr.com/7394/9433718544_4465a655f5_o.jpg',
+    'http://farm4.staticflickr.com/3801/9433719914_3f99ecbb9e_o.jpg',
+    'http://farm4.staticflickr.com/3793/9433720854_bff38cff4a_o.jpg',
+    'http://farm8.staticflickr.com/7282/9430949389_b4b5e7fa92_o.jpg',
+    'http://farm4.staticflickr.com/3717/9433724628_32424c4af3_o.jpg',
+    'http://farm8.staticflickr.com/7353/9430955835_623865e38c_o.jpg',
+]
